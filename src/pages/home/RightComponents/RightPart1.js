f33410fa0bda226276d09e897b8b8a5b81f4b063
codeManeesha <maneeshasingh19jun@gmail.com>
2023-04-28 12:40:33 +0530

    changed in right section

--- src/pages/home/RightComponents/RightPart1.js
+++ src/pages/home/RightComponents/RightPart1.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import RightPart3 from './RightPart3'
-import RightPart2 from './RightPart2'
-import style from './part1.module.css'
-
-
+import React from "react";
+import style from "./rightSection.module.css";
+import RightPart3 from "./RightPart3";
+import RightPart2 from "./RightPart2";
 
 function RightPart1() {
   return (
-
-    <div >
-
-      {/* RightSection1 */}
-      <div className={style.search}>
-        
+    <div>
+      <div className={style.RightSection}>
+        {/* RightPart1 */}
+        <div className={style.searchBar}>
+          <i class="fa fa-search" aria-hidden="true"></i>
           <input type="text" placeholder="Search Tweeter" />
-        
-       
+        </div>
       </div>
-      <RightPart2/>
+      <RightPart2 />
       <RightPart3 />
-
-
-
     </div>
-  )
+  );
 }
-export default RightPart1
+
+export default RightPart1;
--- src/pages/home/RightComponents/RightPart2.js
+++ src/pages/home/RightComponents/RightPart2.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import style from './rightSection.module.css'
 
+
 function RightPart2() {
     return (
         <div className={style.trendbox}>
 
             <h2>What's trending</h2>
-            <div className={style.trend} >
-               
-           
+
 
             <div className={style.trend} >
+
                 <p>Trending in World</p>
                 <h4>#trending</h4>
                 <p>4,500Tweets</p>
@@ -29,11 +29,11 @@ function RightPart2() {
                 <p>Trending in World</p>
                 <h4>#trending</h4>
                 <p>4,500Tweets</p>
+
+
             </div>
-            
-</div>
-           
-<h5>show more</h5>
+
+            <h5>show more</h5>
 
 
 
